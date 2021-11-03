46085a4edb9357d9bd496d0a08da0c5512a73e42
lsoaress <lucas.carvalho@bandtec.com.br>
2021-11-03 16:00:53 -0300

    html/css/js da tela login e as imagens do login

---
+++ js/login-main.js
@@ -0,0 +1,20 @@
+function validar(){
+    var usuario = inp_usuario.value;
+    var senha = inp_senha.value;
+
+    if(usuario.indexOf("@")== -1){
+        alert('Usuario invalido, falta @')        
+    }
+    else if(usuario.indexOf(".com")== -1){
+        alert('Usuário inválido, falta .com')
+    }
+    else if(senha.lenght < 5){
+        alert('Senha muito curta')
+    }
+    else if(senha.lenght > 15){
+        alert('Senha muito grande')
+    }
+    else{
+        formulario_login.action = url.replace('login.html', 'dashboard.html');
+    }
+}
