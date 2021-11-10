da91c7431c9ed8e63419bfe97cd19bb5dd94b47c
lsoaress <lucas.carvalho@bandtec.com.br>
2021-11-10 00:15:39 -0300

    logos do menu

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+
+
+function hide_menu(){
+
+    document.getElementsByClassName
+    if(img_menu.style.display == block){
+        $('.img_menu').css('display', 'none')
+        $('.h5_menu').css('display', 'none')
+    }
+    else if(img_menu.display == 'none'){
+        $('.img_menu').css('display', 'block')
+        $('.h5_menu').css('display', 'block')
+    }
+}
