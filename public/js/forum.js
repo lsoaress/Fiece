d9ebed3063eed688ffe3cc562e927f439e709325
lsoaress <lucas.carvalho@bandtec.com.br>
2021-11-12 11:55:12 -0300

    Adição do fórum arrumado, css arrumado e js arrumado

---
+++ public/js/forum.js
@@ -0,0 +1,37 @@
+function show_menu() {
+
+    var display = container.style.display;
+    
+    if (display != "inline-block") {
+            container.style.display = "inline-block";
+            sidebar.style.background = '#252525';
+            container.style.transition = "all 1.5s ease";
+        }
+    if (display == 'inline-block') {
+        container.style.display = 'none';
+        sidebar.style.background = '#25252500'
+        container.style.transition = "all 1.5s ease";
+    }
+}
+
+// Validações :
+
+function validarSessao() {
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+
+    if (email != null && nome != null) {
+        b_usuario.innerHTML = nome;
+
+    } else {
+        window.location = "../index.html";
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../index.html";
+}
+
