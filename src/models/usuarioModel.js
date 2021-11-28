1c23b92b2652d24e5e3c22895e78d741c4767ab0
lsoaress <lucas.carvalho@bandtec.com.br>
2021-11-28 01:12:07 -0300

    criptografia das senha do banco de dados

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = SHA2('${senha}',224);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -21,7 +21,7 @@ function entrar(email, senha) {
 function cadastrar(nome, email, senha, cep, estado) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha, cep, estado) VALUES ('${nome}', '${email}', '${senha}', '${cep}', '${estado}');
+        INSERT INTO usuario (nome, email, senha, cep, estado) VALUES ('${nome}', '${email}', SHA2('${senha}',224), '${cep}', '${estado}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
